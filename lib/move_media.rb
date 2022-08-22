adcd6f2f9ee7603ae5de7598308f94f84b703bbb
Mike Slinn <mslinn@mslinn.com>
2022-08-22 09:13:34 -0400

    -

--- lib/move_media.rb
+++ lib/move_media.rb
@@ -25,11 +25,6 @@ def move_thumbnails(source, destination, video_filename)
   end
 end
 
-def sony_thumbnails(source, stem)
-  thumb_source_names = "#{source}/PRIVATE/M4ROOT/THMBNL/#{stem}*.jpg"
-  Dir[thumb_source_names]
-end
-
 def move_and_rename(old_path, new_path)
   File.rename old_path, new_path
 end
@@ -50,11 +45,23 @@ def scan_for_highest_seq(destination)
   seq + 1
 end
 
+# [ /mnt/h/PRIVATE/M4ROOT/THMBNL/C0001T01.JPG ]
+# @return [[String]] JPG filenames on memory card
+def sony_thumbnails(source, stem)
+  thumb_source_names = "#{source}/PRIVATE/M4ROOT/THMBNL/#{stem}*.JPG"
+  Dir[thumb_source_names]
+end
+
 def source_bash(dos_drive)
   match_data = dos_drive.match(/^([a-zA-Z]):/)
   raise "#{dos_drive}  is an invalid DOS drive specification." unless match_data && match_data.length == 2
 
   "/mnt/#{match_data[1].downcase}"
+end
+
+# /mnt/h/PRIVATE/M4ROOT/CLIP/C0001.MP4
+def video_filenames(source)
+  Dir["#{source}/**/*.MP4"].sort
 end
 
 def main
@@ -67,16 +74,8 @@ def main
 
   seq = scan_for_highest_seq destination
 
-  # /mnt/h/PRIVATE/M4ROOT/CLIP/C0001.MP4
-  video_filenames = Dir["#{source}/**/*.MP4"].sort
-  # p video_filenames
-
-  # /mnt/h/PRIVATE/M4ROOT/THMBNL/C0001T01.JPG
-  image_filenames = Dir["#{source}/**/*.JPG"].sort
-  # p image_filenames
-
   # Destination files are yyyy-mm-dd_topic_01234.{mp4,jpg}
-  video_filenames.each do |fn|
+  video_filenames(source).each do |fn|
     filename = File.basename(fn, '.*')
 
     new_name = "#{topic}_#{Date.today}_#{seq}"
--- spec/move_media_spec.rb
+++ spec/move_media_spec.rb
@@ -5,11 +5,11 @@ require_relative '../lib/move_media'
 RSpec.describe(MoveMediaVersion) do
   include MoveMediaVersion
 
-  let(:config) { instance_double('Configuration') }
+  # let(:config) { instance_double('Configuration') }
 
-  it 'returns the usual Ruby value' do
-    array = [1, 2, 3, 4, 5]
-    expect(move_media(array, -1)).to eq(5)
-    expect(move_media([], 1)).to raise_exception
+  it 'finds thumbnails for video file stem C0001' do
+    source = 'spec/fixtures'
+
+    expect(sony_thumbnails(source, 'C0001').length).to eq(4)
   end
 end
